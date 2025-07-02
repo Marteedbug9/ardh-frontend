b67aa879b4ddcde82b3d4c8527a401f07bb14ae1
Marteedbug9 <martyveson2@email.com>
2025-07-02 18:52:39 -0400

    Add valid next-i18next config

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr', 'en', 'es', 'ht'],
+    localeDetection: true,
+  },
+  // Optionnel : dossier où sont stockées les traductions
+  localePath: './public/local',
+}
